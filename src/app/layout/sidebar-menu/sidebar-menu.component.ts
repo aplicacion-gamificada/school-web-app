72e5d255505816d5d7780c9dc09897f62ebe9cb3
Sofia Alfaro <alfaros.sofia@gmail.com>
2025-06-18 23:20:10 -0500

    Add: Home Structure

---
+++ src/app/layout/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './sidebar-menu.component.html',
+  styleUrl: './sidebar-menu.component.scss'
+})
+export class SidebarMenuComponent {
+menuItems = [
+  { label: 'Inicio', icon: 'images/icon-inicio.png', route: '/home' },
+  { label: 'Mis Aulas', icon: 'images/icon-aulas.png', route: '/classrooms' },
+  { label: 'Estudiantes', icon: 'images/icon-logro.png', route: '/students' },
+  { label: 'Reportes', icon: 'images/icon-progress.png', route: '/reports' },
+  { label: 'Logros', icon: 'images/icon-logros.png', route: '/achievements' }
+];
+
+  user = {
+    name: 'Luis Valdivia',
+    avatar: 'images/icon-avatar-2.png' 
+  };
+}
